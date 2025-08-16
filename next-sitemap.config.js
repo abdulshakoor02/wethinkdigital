f6a915051a5229e3bbef74f054ba862003c4c37b
abdulshakoor02 <samunleash1@yahoo.com>
2025-08-17 02:37:56 +0400

    added next site map config

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.wethinkdigital.solutions", // replace with your domain
+  generateRobotsTxt: true, // generate robots.txt automatically
+  sitemapSize: 5000, // optional, max URLs per sitemap
+  changefreq: "daily", // optional
+  priority: 0.7, // optional
+};
